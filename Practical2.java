71899334af8108f08afc4f9a1b29250fc4abffe0
Asmit1211 <149766685+Asmit1211@users.noreply.github.com>
2024-12-29 13:43:12 +0530

    Practical 2: Sum of two numbers
    Description:
    
    Importing Scanner:
    The program uses the Scanner class from the java.util package to allow the user to input numbers.
    Class and Method Declaration:
    
    The class is named Practical2.
    The main method is the entry point of the program.
    Taking Input:
    
    A Scanner object named input is created to read user input from the console.
    The program prompts the user with: "Enter the first number :". The inputted integer is stored in the variable num1.
    Similarly, the user is prompted with: "Enter the second number :". The second inputted integer is stored in the variable num2.
    Calculating the Sum:
    
    The program calculates the sum of num1 and num2 and stores the result in the variable sum.

---
+++ Practical2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Practical2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number :");
+        int num1 = input.nextInt();
+        System.out.print("Enter the second number : ");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println();
+        System.out.println("Sum: "+sum);
+    }
+    
+}
